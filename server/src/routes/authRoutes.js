f563c082a0a81606f8b929120e77651a51a088c4
konetichandrakant <konetichandrakant@gmail.com>
2025-05-12 16:45:30 -0400

    Added logout button on profile page

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { loginController, registerController } = require('../controller/auth');
+const { loginController, logoutController, registerController } = require('../controller/auth');
 
 router.post('/login', loginController);
 router.post('/register', registerController);
+router.post('/logout', logoutController);
 
 exports.authRouter = router;
